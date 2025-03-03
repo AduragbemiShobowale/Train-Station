a3d2c7757c6523f2dfaff0e6903ec61a9d26db5b
Mayokun Oyegbile <mayokunoyegbile94@gmail.com>
2025-03-03 12:31:21 +0100

    working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import TrainLogo from "../assets/icon/TrainLogo.png";
+import { FaBars } from "react-icons/fa";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default NavBar
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div>
+        <img
+          className="logo"
+          src={TrainLogo}
+          alt="NIGERIAN 
+          RAILWAY 
+          CORPORATION"
+        />
+      </div>
+
+      <div className={`navigate ${isOpen ? "open" : ""}`}>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/table">Time Table</a>
+        <a href="faq">FAQS</a>
+        <a href="contact">Contact Us</a>
+
+        <button className="register-button">Register</button>
+        <button className="signin-button">Sign In</button>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <FaBars />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
