0597c06712c793defa002b900da783acce83ad8d
Mayokun Oyegbile <mayokunoyegbile94@gmail.com>
2025-03-05 12:41:55 +0100

    contact us hero section

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "../pages/ContactUsPage.css";
+// import motionTrain from "../assets/image/blurtrain.png.png";
 
 const ContactUsPage = () => {
   return (
-    <div>ContactUsPage</div>
-  )
-}
+    <div className="mainContainer">
+      <div className="contact-text">
+        <h1>Contact Us</h1>
+      </div>
+      <p className="content">
+        Safe, comfortable, and reliable train services across the nation.
+        Experience the future of Nigerian transportation, today.
+      </p>
+    </div>
+  );
+};
 
-export default ContactUsPage
+export default ContactUsPage;
