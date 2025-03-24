3baccfd7bcc7ca1def635ea11de246b8afb30e4b
AduragbemiShobowale <shobowaleaduragbemi24@gmail.com>
2025-03-24 13:37:15 +0100

    adjusted homepage hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ function HomePage() {
     <div>
       <div className="relative">
         <div className="mainCont">
-          <div className="max-w-7xl mx-auto flex flex-col justify-center items-center text-center md:text-start py-20 px-6 md:px-12 gap-5 md:justify-start md:items-start ">
+          <div className="max-w-7xl mx-auto flex flex-col justify-center items-center md:text-start py-20 px-6 md:px-12 gap-5 md:justify-start md:items-start ">
             <h1 className="lg:text-[56px] text-[28px] text-white font-bold leading-tight max-w-[800px]">
               Journey Through Nigeria's Heart by Rail
             </h1>
@@ -28,7 +28,6 @@ function HomePage() {
         <TrainInfo />
         <NewsUpdates />
         <RailwayCompanion />
-        
       </div>
     </div>
   );
